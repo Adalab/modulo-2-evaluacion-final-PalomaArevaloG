7ea790f482b0b893093c87bf2a75b3b9f8ea5d0f
Paloma <paloma.arevalog@gmail.com>
2021-09-15 12:42:44 +0200

    ejercicio done

--- src/js/main.js
+++ src/js/main.js
@@ -2,22 +2,23 @@
 'use strict';
 
 // Variables
-const input = document.querySelector('.js_input');
-const btnSearch = document.querySelector('.js_btn-search');
-let favorites = document.querySelector('.js_fav');
-let list = document.querySelector('.js_list');
-
-const btnReset = document.querySelector('.js_btn-reset');
+const input = document.querySelector('.js_input'); // me traigo el input
+const btnSearch = document.querySelector('.js_btn-search'); //me traigo el botón de favoritos
+let favorites = document.querySelector('.js_fav'); // me traigo la lista de fav
+let list = document.querySelector('.js_list'); // me traigo la lista de rtdos
+const btnReset = document.querySelector('.js_btn-reset'); //me traigo el boton de reset
+const numero = document.querySelector('.js-results');
 
 //variable para el array que de el fetch
 let arrayShows = [];
 //variable de favoritos
 let arrayFavs = [];
 
+let arrayNumero = [2, 5, 9];
 //Función para búsqueda Fetch
 
 function handleSearch(ev) {
-    ev.preventDefault();
+    ev.preventDefault(); // para que nos e recargue automaticamnete
     let shows = input.value;
     fetch(`//api.tvmaze.com/search/shows?q=${shows}`)
         .then((response) => response.json())
@@ -28,44 +29,66 @@ function handleSearch(ev) {
             setInLocalStorage();
         });
 }
+
+function handleNumero() {
+    for (const number of arrayNumero) {
+        if (arrayShows.length >= number) {
+            console.log(
+                `El numero de resultados es: ${arrayShows.length} y es mayor que ${number}`
+            );
+        } else {
+            console.log(
+                `El numero de resultados es: ${arrayShows.length} y es menor que ${number}`
+            );
+        }
+    }
+}
+numero.addEventListener('click', handleNumero);
 //Función para pintar la búsqueda
 function paintShows() {
     let html = '';
     //si el elemento que quiero pintar está en arrayfav añadele la clase favorite
     let favClass = '';
     for (const data of arrayShows) {
-        const isFav = isFavorite(data);
+        const isFav = isFavorite(data); // obtengo lo que me devuelve la funcion que comprueba si es favorito
         if (isFav) {
             favClass = 'favorite';
-            favorites.innerHTML += html;
+            favorites.innerHTML += html; // si lo es le añado la clase y lo meto en el html de favoritos
         } else {
             favClass = '';
         }
 
         if (data.show.image === null) {
-            html += `<li id="${data.show.id}" class="list-show js_list-show ${favClass}">`;
-            html += `<div class="result js_result ">`;
-            html += `<h2 class="js_showName showName">${data.show.name}</h2>`;
+            //si no tiene imagen pinto este html
+            html += ` <li id = "${data.show.id}"
+            class = "list-show js_list-show ${favClass}"> `;
+            html += ` <div class = "result js_result "> `;
+            html += ` <h2 class = "js_showName showName"> ${data.show.name} </h2>`;
+            html += `<p>${data.show.premiered}`;
             html += `<img class="js-image img" src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"/>`;
             html += `</div></li>`;
         } else {
+            //si tiene pinto este otro
             html += `<li id="${data.show.id}" class="list-show js_list-show ${favClass}">`;
             html += `<div class="result js_result ">`;
             html += `<h2 class="js_showName showName">${data.show.name}</h2>`;
+            html += `<p>${data.show.premiered}`;
             html += `<img class="js-image img" src="${data.show.image.medium}"/>`;
             html += `</div></li>`;
         }
     }
-    list.innerHTML = html;
-    listenShows();
+    numero.innerHTML = arrayShows.length;
+    list.innerHTML = html; // meto la busqueda en la lista del html
+    listenShows(); //después de modificar el html escucho de nuevo los eventos
 }
 //Función para coger los favoritos
 function handleFavs(ev) {
+    // obtengo el id de la serie clickada
     const selectedShow = parseInt(ev.currentTarget.id);
-
-    const objectClicked = arrayShows.find((fav) => {
-        return fav.show.id === selectedShow;
-    });
+    //busco la serie clickada en el arrayshows
+    const objectClicked = arrayShows.find((data) => {
+        return data.show.id === selectedShow;
+    }); //busco si la serie clicada esta en arrayfavs
     const favoritesFound = arrayFavs.findIndex((fav) => {
         return fav.show.id === selectedShow;
     });
@@ -73,35 +96,39 @@ function handleFavs(ev) {
         arrayFavs.push(objectClicked);
         // añadir con un push la serie clickada buscar en arrayShows el elemento de array shows que tenga ese id y añadirlo a arrayFavs
     } else {
+        //si no lo saco de favoritos con splice con el indice del elto que quiiero borrar y que solo es un elto
         arrayFavs.splice(favoritesFound, 1);
     }
-    paintShows();
-    paintFavs();
+    paintShows(); //vuelvo a pintar la busqueda
+    paintFavs(); //vulvo a pintar los que son favs
 }
 
-// Función para coger los favoritos
+// función escucho eventos sobre los elementos del array
 function listenShows() {
+    //selecciono los elementos pintados en la lisat
     const listShows = document.querySelectorAll('.js_list-show');
-
+    //recorro el arrayShows y escucho que al hacer click me seleccione que sea fav
     for (const showEl of listShows) {
         showEl.addEventListener('click', handleFavs);
     }
 }
 //Función que verifica si es favorito
 function isFavorite(data) {
+    //compruebo que la serie está en favoritos comparando los datos del arrayshows con los del array fav
     const favoriteFound = arrayFavs.find((fav) => {
         return fav.show.id === data.show.id;
     });
     if (favoriteFound === undefined) {
-        return false;
+        return false; //si no está en favoritos
     } else {
-        return true;
+        return true; //si está en favoritos
     }
 }
 //Función para pintar los favoritos
 function paintFavs() {
-    let htmlFav = '';
+    let htmlFav = ''; //creo una vble para meter el html de fav
     for (const fav of arrayFavs) {
+        //para la constante fav de arrayfavs creo html dependiendo de si tien o no imagen
         if (fav.show.image === null) {
             htmlFav += `<li id="${fav.show.id}" class="list-show js_list-show">`;
             htmlFav += `<div class="result js_result ">`;
@@ -116,13 +143,15 @@ function paintFavs() {
             htmlFav += `</div></li>`;
         }
     }
-    favorites.innerHTML = htmlFav;
+    favorites.innerHTML = htmlFav; //meto los favoritos en el html
     setInLocalStorage(); //almaceno favs en localstorage
-    clickedIcon();
+    clickedIcon(); //ecsucho el icono de favs
 }
 //funcion para almacenar en local storage
 function setInLocalStorage() {
+    //transformo a string el array de favs
     const stringFavs = JSON.stringify(arrayFavs);
+    //añadimos alo local storage el arrayfav en string
     localStorage.setItem('arrayFavs', stringFavs);
 }
 //funcion para buscar en local storage
@@ -137,7 +166,7 @@ function getLocalStorage() {
     } else {
         const arrayLocalStorage = JSON.parse(localStorageFavs);
         arrayFavs = arrayLocalStorage;
-        paintFavs();
+        paintFavs(); //cada vez que modifico pinto en favoritas
     }
 }
 
@@ -147,13 +176,14 @@ function getLocalStorage() {
 function clickedIcon() {
     const deleteIcons = document.querySelectorAll('.js_icon');
     for (const deleteIcon of deleteIcons) {
+        //si pulso el icono aplico la funcion removefavs
         deleteIcon.addEventListener('click', removeFav);
     }
 }
 
 //funcion para quitar de favoritos al pulsar el icono
 function removeFav(event) {
-    const icon = parseInt(event.currentTarget.id);
+    const icon = parseInt(event.currentTarget.id); //cojo el evento del icono
     const favFound = arrayFavs.findIndex((fav) => fav.id === icon);
 
     arrayFavs.splice(favFound, 1);
@@ -165,7 +195,7 @@ function removeFav(event) {
 
 //funcion para quitar todos los favoritos al pulsar reset
 function removeAllFavs() {
-    arrayFavs = [];
+    arrayFavs = []; //lo dejo vacio
     setInLocalStorage();
     paintFavs();
     paintShows();
